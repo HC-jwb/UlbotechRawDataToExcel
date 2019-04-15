974a9cbec78702dc47d384f134102afbde86910c
변재욱 <>
2019-04-16 00:17:54 +0900

    when remove unnecessary data including colon

--- src/demo/main/Main.java
+++ src/demo/main/Main.java
@@ -35,25 +35,25 @@ public class Main {
 					if (lineCharArray[i] == ',') {
 						switch (flag) {
 						case "GPS":
-							data.setGps_location(line.substring(previousCommaIndex + 1, i));
+							data.setGps_location(line.substring(previousCommaIndex + 5, i));
 							break;
 						case "LBS":
-							data.setLbs_location(line.substring(previousCommaIndex + 1, i));
+							data.setLbs_location(line.substring(previousCommaIndex + 5, i));
 							break;
 						case "STT":
-							data.setStt_data(line.substring(previousCommaIndex + 1, i));
+							data.setStt_data(line.substring(previousCommaIndex + 5, i));
 							break;
 						case "MGR":
-							data.setMgr_data(line.substring(previousCommaIndex + 1, i));
+							data.setMgr_data(line.substring(previousCommaIndex + 5, i));
 							break;
 						case "ADC":
-							data.setAdc_data(line.substring(previousCommaIndex + 1, i));
+							data.setAdc_data(line.substring(previousCommaIndex + 5, i));
 							break;
 						case "OBD":
-							data.setObc_data(line.substring(previousCommaIndex + 1, i));
+							data.setObc_data(line.substring(previousCommaIndex + 5, i));
 							break;
 						case "FUL":
-							data.setFul_data(line.substring(previousCommaIndex + 1, i));
+							data.setFul_data(line.substring(previousCommaIndex + 5, i));
 							break;
 						case "OAL":
 							break;
@@ -64,9 +64,9 @@ public class Main {
 						}
 
 						if (indexOfComma == 1) { // IMEI
-							data.setDevice_imei(line.substring(previousCommaIndex + 1, i));
+							data.setDevice_imei(line.substring(previousCommaIndex + 5, i));
 						} else if (indexOfComma == 2) { // Time
-							data.setCurrent_time(line.substring(previousCommaIndex + 1, i));
+							data.setCurrent_time(line.substring(previousCommaIndex + 5, i));
 						} else {
 							switch (line.substring(i + 1, i + 4)) {
 							case "GPS":
@@ -91,7 +91,7 @@ public class Main {
 								flag = "FUL";
 								break;
 							case "EVT":
-								data.setEvt_data(line.substring(i + 1, lineCharArray.length));
+								data.setEvt_data(line.substring(i + 5, lineCharArray.length));
 								break;
 							case "OAL":
 								flag = "OAL";
@@ -110,18 +110,18 @@ public class Main {
 				dataList.add(data);
 			}
 
-//			for (int i = 0; i < dataList.size(); i++) {
-//				System.out.println("GPS : " + dataList.get(i).getGps_location());
-//				System.out.println("LBS : " + dataList.get(i).getLbs_location());
-//				System.out.println("STT : " + dataList.get(i).getStt_data());
-//				System.out.println("MGR : " + dataList.get(i).getMgr_data());
-//				System.out.println("ADC : " + dataList.get(i).getAdc_data());
-//				System.out.println("OBD : " + dataList.get(i).getObd_data());
-//				System.out.println("FUL : " + dataList.get(i).getFul_data());
-//				System.out.println("EVT : " + dataList.get(i).getEvt_data());
-//			}
+			for (int i = 0; i < dataList.size(); i++) {
+				System.out.println("GPS : " + dataList.get(i).getGps_location());
+				System.out.println("LBS : " + dataList.get(i).getLbs_location());
+				System.out.println("STT : " + dataList.get(i).getStt_data());
+				System.out.println("MGR : " + dataList.get(i).getMgr_data());
+				System.out.println("ADC : " + dataList.get(i).getAdc_data());
+				System.out.println("OBD : " + dataList.get(i).getObd_data());
+				System.out.println("FUL : " + dataList.get(i).getFul_data());
+				System.out.println("EVT : " + dataList.get(i).getEvt_data());
+			}
 
-			System.out.println(dataList.size());
+			
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
